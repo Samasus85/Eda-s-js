a0598d96e59d4f0f8525a6b83c8a637a1c8c6bae
Samasus85 <89699068+Samasus85@users.noreply.github.com>
2021-11-07 11:48:21 +0300

    Add files via upload
    trird

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,57 @@
-// const restourant = 'food-band';
+
+const cardsMenu = document.querySelector('.cards-menu')
+
+const changeTitle = (restaurant) => {
+    const restaurantTitle = document.querySelector('restaurant-title')
+    // console.log(restaurantTitle);
+    restaurantTitle.textContent = restaurant.name
+}
+
+// console.log(cardsMenu);
 
 
 const renderItems = (data) => {
-    console.log(data);
-}
 
-// fetch(`./db/${restourant}.json`)
-fetch(`./db/tanuki.json`)
-    .then((response) => response.json())
-    .then((data) => {
-        renderItems(data)
+    data.forEach(({ description, id, image, name, price }) => {
+        const card = document.createElement('div')
+        // console.log(description);
+        card.classList.add('card')
+
+        card.innerHTML = `
+            <img src="${image}" alt="${name}" class="card-image" />
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title card-title-reg">${name}</h3>
+                </div>
+                <div class="card-info">
+                    <div class="ingredients">${description}</div>
+                </div>
+                <div class="card-buttons">
+                    <button class="button button-primary button-add-cart">
+                        <span class="button-card-text">В корзину</span>
+                        <span class="button-cart-svg"></span>
+                    </button>
+                    <strong class="card-price-bold">${price}</strong>
+                </div>
+            </div>
+        `
+
+        cardsMenu.append(card)
     })
+}
+if (localStorage.getItem('restaurant')) {
+    const restaurant = JSON.parse(localStorage.getItem('restaurant'))
+
+    changeTitle(restaurant)
+
+    fetch(`./db/${restaurant.products}`)
+        .then((response) => response.json())
+        .then((data) => {
+            renderItems(data)
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+} else {
+    window.location.href = '/'
+}
